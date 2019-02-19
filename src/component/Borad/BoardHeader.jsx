efee8ec97eb80cb40336984f6b08b72ec4aee290
Tammam <a.tamam@asfarelectronics.com>
2019-02-19 17:32:25 +0200

    ...

--- src/component/Borad/BoardHeader.jsx
+++ src/component/Borad/BoardHeader.jsx
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 
 export default class BoardHeader extends Component {
+	constructor() {
+		super();
+		this.state = {
+			edit: false
+		};
+	}
+
+	edit = () => {
+		this.setState(({ edit }) => ({ edit: !edit }));
+	};
+	nameChange = (e) => {
+		console.log(e.target.value);
+	};
+
 	render() {
-		const style = `${this.props.color} white-text center-align card-header`;
+		const style = `${this.props.color ? this.props.color : 'blue'} white-text card-header`;
 		return (
 			<div className={style}>
-				<span className="card-title">{this.props.name}</span>
+				{this.state.edit ? (
+					<input className="white" value={this.props.name} onChange={this.nameChange} />
+				) : (
+					<span className="card-title left-align">{this.props.name}</span>
+				)}
+				<a className="waves-effect waves-light btn right-align" onClick={this.edit}>
+					{this.state.edit ? 'done' : 'edit'}
+				</a>
 			</div>
 		);
 	}
